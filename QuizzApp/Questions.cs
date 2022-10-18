33398e564cc213a504675f8d7e0157e2ee801775
OierGman <75170798+OierGman@users.noreply.github.com>
2022-10-18 23:53:24 +0100

    class generation

---
+++ QuizzApp/Questions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizzApp
+{
+    internal class Questions
+    {
+        public class Result
+        {
+            public string category { get; set; }
+            public string type { get; set; }
+            public string difficulty { get; set; }
+            public string question { get; set; }
+            public string correct_answer { get; set; }
+            public List<string> incorrect_answers { get; set; }
+        }
+        public class Root
+        {
+            public int response_code { get; set; }
+            public List<Result> results { get; set; }
+        }
+    }
+}
---
+++ QuizzApp/QuizEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace QuizzApp
+{
+    public class QuizEngine
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public static async Task Main()
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                client.BaseAddress = new Uri("https://opentdb.com/");
+                HttpResponseMessage response = await client.GetAsync("api.php?amount=10&category=9&difficulty=easy");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+                Questions.Root myDeserializedClass = JsonSerializer.Deserialize<Questions.Root>(responseBody);
+                MessageBox.Show(myDeserializedClass.results.ToString());
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
