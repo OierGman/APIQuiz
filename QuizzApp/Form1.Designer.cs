40c9b1c1aafbe093a99b150cd3b6525105f7fa53
jpinn97 <JP878@canterbury.ac.uk>
2022-10-19 21:43:34 +0100

    Quiz builder UI template partially completed.

--- QuizzApp/Form1.Designer.cs
+++ QuizzApp/Form1.Designer.cs
@@ -31,13 +31,32 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.categoriesLabel = new System.Windows.Forms.Label();
+            this.categoriesCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.quizLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 36);
+            this.button1.Location = new System.Drawing.Point(69, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 54);
+            this.button1.Size = new System.Drawing.Size(118, 72);
             this.button1.TabIndex = 0;
             this.button1.Text = "Single Player";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,25 +64,212 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(60, 96);
+            this.button2.Location = new System.Drawing.Point(69, 128);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 53);
+            this.button2.Size = new System.Drawing.Size(118, 71);
             this.button2.TabIndex = 1;
             this.button2.Text = "Two Player";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.quizLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.categoriesCheckedListBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.categoriesLabel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(373, 48);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72.09303F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.90698F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 270F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(515, 523);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Difficulty";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 399);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(251, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Miscellanous Effects";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.checkBox4, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.checkBox5, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.checkBox6, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 425);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(159, 94);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(3, 65);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(101, 24);
+            this.checkBox4.TabIndex = 2;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(3, 34);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(101, 24);
+            this.checkBox5.TabIndex = 1;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(3, 3);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(119, 24);
+            this.checkBox6.TabIndex = 0;
+            this.checkBox6.Text = "Timed Events";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Easy";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(3, 34);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Medium";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 65);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(64, 24);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Hard";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.checkBox3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox1, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(260, 425);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(159, 95);
+            this.tableLayoutPanel2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(260, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Difficulty";
+            // 
+            // categoriesLabel
+            // 
+            this.categoriesLabel.AutoSize = true;
+            this.categoriesLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.categoriesLabel.Location = new System.Drawing.Point(260, 93);
+            this.categoriesLabel.Name = "categoriesLabel";
+            this.categoriesLabel.Size = new System.Drawing.Size(252, 36);
+            this.categoriesLabel.TabIndex = 2;
+            this.categoriesLabel.Text = "Categories";
+            // 
+            // categoriesCheckedListBox
+            // 
+            this.categoriesCheckedListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.categoriesCheckedListBox.FormattingEnabled = true;
+            this.categoriesCheckedListBox.Location = new System.Drawing.Point(260, 132);
+            this.categoriesCheckedListBox.Name = "categoriesCheckedListBox";
+            this.categoriesCheckedListBox.Size = new System.Drawing.Size(252, 264);
+            this.categoriesCheckedListBox.TabIndex = 1;
+            // 
+            // quizLabel
+            // 
+            this.quizLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.quizLabel, 3);
+            this.quizLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.quizLabel.Location = new System.Drawing.Point(3, 0);
+            this.quizLabel.Name = "quizLabel";
+            this.quizLabel.Size = new System.Drawing.Size(509, 93);
+            this.quizLabel.TabIndex = 9;
+            this.quizLabel.Text = "Quiz Builder!";
+            this.quizLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -72,5 +278,20 @@
 
         private Button button1;
         private Button button2;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private Label label5;
+        private TableLayoutPanel tableLayoutPanel3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private Label quizLabel;
+        private CheckedListBox categoriesCheckedListBox;
+        private Label categoriesLabel;
+        private Label label3;
+        private TableLayoutPanel tableLayoutPanel2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
     }
 }
