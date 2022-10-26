2e16a9900f2d44b35c4d5771f5da7098bdeb9adf
Ryan Mitchell Judd <RJ213@canterbury.ac.uk>
2022-10-26 19:18:01 +0100

    Declared variables for picture questions within its own class

--- QuizzApp/PictureQuestions.cs
+++ QuizzApp/PictureQuestions.cs
@@ -8,6 +8,17 @@ namespace QuizzApp
 {
     public class PictureQuestions
     {
+        //Declaring a list to store each picture question, and relevant variables of each
+        public static List<PictureQuestions> PictureQuestion = new List<PictureQuestions>();
+
+        //Variables for each question
+        public Image Image;
+        public string Category;
+        public string Type;
+        public string Difficulty;
+        public string Question;
+        public string CorrectAnswer;
+        public List<string> IncorrectAnswers;
         
     }
 }
